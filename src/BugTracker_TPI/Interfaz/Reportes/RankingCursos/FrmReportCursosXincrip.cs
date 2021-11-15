81d62785fe6a7091224fa67e2e4038c70737fd5f
nachoenrique <nacho.enrique.97@gmail.com>
2021-11-15 12:36:54 -0300

    Top de ranking terminado

--- src/BugTracker_TPI/Interfaz/Reportes/RankingCursos/FrmReportCursosXincrip.cs
+++ src/BugTracker_TPI/Interfaz/Reportes/RankingCursos/FrmReportCursosXincrip.cs
@@ -20,18 +20,34 @@ namespace BugTracker_TPI.Interfaz.Reportes.RankingCursos
 
         private void FrmReportCursosXincrip_Load(object sender, EventArgs e)
         {
+            cmbTop.SelectedIndex = 0;
             cargarReporte();
 
             this.rpv_cursos.RefreshReport();
         }
 
+
         private void cargarReporte()
         {
-            string sql_string = "select c.id_curso, c.nombre, count(*) as cant_inscriptos " +
+            var top = 0;
+
+            if (cmbTop.SelectedIndex != 0)
+            {
+                if(cmbTop.SelectedIndex == 1)
+                {
+                     top = 5;
+                } else
+                {
+                    top = 10;
+                }
+            } else { top = 3; }
+
+
+            string sql_string = "select top " + top.ToString() + " c.id_curso, c.nombre, count(*) as cant_inscriptos " +
                 "from Cursos c inner join UsuariosCurso uc on " +
                 "c.id_curso = uc.id_curso " +
                 "group by c.id_curso, c.nombre " +
-                "order by cant_inscriptos desc";
+                "order by cant_inscriptos desc ";
 
             rpv_cursos.LocalReport.DataSources.Clear();
             rpv_cursos.LocalReport.DataSources.Add(new ReportDataSource("DSReportes",DataManager.GetInstance().ConsultaSQL(sql_string)));
@@ -41,6 +57,11 @@ namespace BugTracker_TPI.Interfaz.Reportes.RankingCursos
         private void reportViewer1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void cmbTop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cargarReporte();
         }
     }
 }
--- src/BugTracker_TPI/Interfaz/Reportes/RankingCursos/FrmReportCursosXincrip.designer.cs
+++ src/BugTracker_TPI/Interfaz/Reportes/RankingCursos/FrmReportCursosXincrip.designer.cs
@@ -34,6 +34,8 @@ namespace BugTracker_TPI.Interfaz.Reportes.RankingCursos
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmReportCursosXincrip));
             this.cursosBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.rpv_cursos = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.cmbTop = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.cursosBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,23 +45,48 @@ namespace BugTracker_TPI.Interfaz.Reportes.RankingCursos
             // 
             // rpv_cursos
             // 
-            this.rpv_cursos.Dock = System.Windows.Forms.DockStyle.Fill;
             reportDataSource1.Name = "DataSet1";
             reportDataSource1.Value = this.cursosBindingSource;
             this.rpv_cursos.LocalReport.DataSources.Add(reportDataSource1);
             this.rpv_cursos.LocalReport.ReportEmbeddedResource = "BugTracker_TPI.Interfaz.Reportes.RankingCursos.Report_CantInscpCursos.rdlc";
-            this.rpv_cursos.Location = new System.Drawing.Point(0, 0);
+            this.rpv_cursos.Location = new System.Drawing.Point(0, 39);
             this.rpv_cursos.Name = "rpv_cursos";
             this.rpv_cursos.ServerReport.BearerToken = null;
-            this.rpv_cursos.Size = new System.Drawing.Size(647, 368);
+            this.rpv_cursos.Size = new System.Drawing.Size(647, 329);
             this.rpv_cursos.TabIndex = 0;
             this.rpv_cursos.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // cmbTop
+            // 
+            this.cmbTop.FormattingEnabled = true;
+            this.cmbTop.Items.AddRange(new object[] {
+            "Top 3",
+            "Top 5",
+            "Top 10"});
+            this.cmbTop.Location = new System.Drawing.Point(514, 12);
+            this.cmbTop.Name = "cmbTop";
+            this.cmbTop.Size = new System.Drawing.Size(121, 21);
+            this.cmbTop.TabIndex = 1;
+            this.cmbTop.SelectedIndexChanged += new System.EventHandler(this.cmbTop_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(482, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ver:";
             // 
             // FrmReportCursosXincrip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(647, 368);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbTop);
             this.Controls.Add(this.rpv_cursos);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmReportCursosXincrip";
@@ -68,6 +95,7 @@ namespace BugTracker_TPI.Interfaz.Reportes.RankingCursos
             this.Load += new System.EventHandler(this.FrmReportCursosXincrip_Load);
             ((System.ComponentModel.ISupportInitialize)(this.cursosBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,5 +106,7 @@ namespace BugTracker_TPI.Interfaz.Reportes.RankingCursos
         private DSReportes dSReportes1;
         private System.Windows.Forms.BindingSource cursosBindingSource;
         private DSReportesTableAdapters.CursosTableAdapter cursosTableAdapter;
+        private System.Windows.Forms.ComboBox cmbTop;
+        private System.Windows.Forms.Label label1;
     }
 }
